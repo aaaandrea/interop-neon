f8505b7b71b1369d36500021ec95a4dda65017bb
Andrea del Rio <adelrio@gmail.com>
2018-04-24 14:25:59 -0700

    add greeting

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -1,13 +1,22 @@
 #[macro_use]
 extern crate neon;
+extern crate num_cpus;
 
 use neon::vm::{Call, JsResult};
-use neon::js::JsString;
+use neon::mem::Handle;
+use neon::vm::Lock;
+use neon::js::{JsString, JsInteger, JsFunction, Object, Value};
+use neon::js::class::{JsClass, Class};
+
+pub struct Greeting {
+    greeting: String,
+}
 
 fn hello(call: Call) -> JsResult<JsString> {
     let scope = call.scope;
     Ok(JsString::new(scope, "hello node\n\n     love, \n      rust").unwrap())
 }
+
 
 register_module!(m, {
     m.export("hello", hello)
