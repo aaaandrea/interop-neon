5ddc67146d199e1009766a4e372d11829cfa43a8
Andrea del Rio <adelrio@gmail.com>
2018-04-24 17:16:19 -0700

    add thread nums

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -3,21 +3,13 @@ extern crate neon;
 extern crate num_cpus;
 
 use neon::vm::{Call, JsResult};
-use neon::mem::Handle;
-use neon::vm::Lock;
-use neon::js::{JsString, JsInteger, JsFunction, Object, Value};
+use neon::js::{JsString, JsInteger, JsFunction, JsNumber, Object, Value};
 use neon::js::class::{JsClass, Class};
 
-pub struct Greeting {
-    greeting: String,
+fn threading_hint(call: Call) -> JsResult<JsNumber> {
+    Ok(JsNumber::new(call.scope, num_cpus::get() as f64))
 }
-
-fn hello(call: Call) -> JsResult<JsString> {
-    let scope = call.scope;
-    Ok(JsString::new(scope, "hello node\n\n     love, \n      rust").unwrap())
-}
-
 
 register_module!(m, {
-    m.export("hello", hello)
+    m.export("threading_hint", threading_hint)
 });
