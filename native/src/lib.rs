b5c02e4552d1671eb624d21ad67faccfe86ff9d3
Andrea del Rio <adelrio@gmail.com>
2018-05-02 16:59:42 -0700

    add neon

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -2,51 +2,96 @@
 extern crate neon;
 extern crate num_cpus;
 extern crate fst;
+extern crate fst_regex;
 
-// write a program that ties that with a very basic usage of the Rust fst library's 
-// Set type. You'll want to expose two classes: one that takes a path on disk,
-// builds an FST there, and exposes a function to JS for adding words and a function
-// for finalizing the structure, and another class that accepts the path of a built
-// FST, loads it, and provides a read-only interface to its contents that can test
-// if a word is in it or not. Functionality-wise, these will approximately wrap
-// SetBulder and Set respectively. For the latter, ideally we'd load the built
-// file via mmap.
+use std::error::Error;
 
+use std::io;
+use std::io::prelude::*;
+use std::fs::File;
 
-use fst::Set;
+use fst::{Streamer, Set, SetBuilder};
+use fst_regex::Regex;
+
 use neon::vm::{Call, JsResult};
 use neon::mem::Handle;
-use neon::js::{JsString, JsInteger, JsFunction, JsNumber, JsValue, JsNull, JsUndefined};
+use neon::js::{JsString, JsInteger, JsFunction, JsNumber, JsValue, JsNull, JsUndefined, Key};
 use neon::js::class::{JsClass, Class};
 
-fn threading_hint(call: Call) -> JsResult<JsNumber> {
-    Ok(JsNumber::new(call.scope, num_cpus::get() as f64))
+pub struct FstSet {
+    pub something: Set,
 }
 
-// example for getting js arguments
-// in JS:
-// require('neon-module').async_method('show me the money', (n) => console.log(n))
-// >> 'show me the money'
+// fn example() -> Result<(), Box<Error>> {
+//     let set1 = Set::from_iter(&["AC/DC", "Aerosmith"])?;
+//     let set2 = Set::from_iter(&["Bob Seger", "Bruce Springsteen"])?;
+//     let set3 = Set::from_iter(&["George Thorogood", "Golden Earring"])?;
+//     let set4 = Set::from_iter(&["Kansas"])?;
+//     let set5 = Set::from_iter(&["Metallica"])?;
+//
+//     Ok(())
+// }
 
-fn async_method(call: Call) -> JsResult<JsUndefined> {
+declare_types! {
+    pub class WriteFst as WriteFst for SetBuilder<Vec<u8>> {
+        init(mut call) {
+            let scope = call.scope;
+            // takes path on disk
 
-	let fn_handle = call.arguments.get(call.scope, 1).unwrap();
-	let arg_handle = call.arguments.get(call.scope, 0).unwrap();
+            let mut build = SetBuilder::memory();
+            // let mut wtr = io::BufWriter::new(File::create("set.fst").unwrap());
+            // let pathway = Set::from_path<P: AsRef<Path>>(path: P) -> Result<Self> {
+            //     raw::Fst::frin_path(path).map(Set);
+            // }
+            Ok(build)
+        }
 
-	if let Some(function) = fn_handle.downcast::<JsFunction>() {
+        method  buildFST(call) {
+            let scope = call.scope;
+            // builds fst on path
+            let mut build = SetBuilder::memory();
+            build.insert("bruce").unwrap();
+            build.insert("clarence").unwrap();
+            build.insert("stevie").unwrap();
 
-		let args: Vec<Handle<JsValue>> = vec![arg_handle];
-		let _ = function.call(call.scope, JsNull::new(), args);
-	}
-
-	Ok(JsUndefined::new())
+            // You could also call `finish()` here, but since we're building the set in
+            // memory, there would be no way to get the `Vec<u8>` back.
+            let bytes = build.into_inner().unwrap();
+            Ok(JsUndefined::new())
+        }
+    }
+    // pub class ReadFst for Set {
+    //     init(mut call) {
+    //         let scope = call.scope;
+    //         // takes path on disk
+    //         // let pathway = Set::from_path<P: AsRef<Path>>(path: P) -> Result<Self> {
+    //         //     raw::Fst::frin_path(path).map(Set);
+    //         // }
+    //         Ok(Set::new())
+    //     }
+    //
+    //     method search(call) {
+    //         let scope = call.scope;
+    //         let set = unsafe { Set::from_path("set.fst").unwrap() };
+    //         let mut stream = set.into_stream();
+    //         let mut keys = vec![];
+    //         while let Some(key) = stream.next() {
+    //             keys.push(key.to_vec());
+    //         }
+    //         assert_eq!(keys, vec![
+    //             "bruce".as_bytes(), "clarence".as_bytes(), "stevie".as_bytes(),
+    //         ]);
+    //         Ok(JsUndefined::new())
+    //     }
+    // }
 }
 
 register_module!(m, {
-	try!(m.export("async_method", async_method));
+    // try!(m.export("example", example));
+    let writeClass: Handle<JsClass<WriteFst>> = try!(WriteFst::class(m.scope));
+    let writeConstructor: Handle<JsFunction<WriteFst>> = try!(writeClass.constructor(m.scope));
+
+	try!(m.exports.set("WriteFst", writeConstructor));
+    // try!(m.export("ReadFst", ReadFst));
 	Ok(())
 });
-
-// register_module!(m, {
-//     m.export("threading_hint", threading_hint)
-// });
