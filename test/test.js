4f7541335a090aed51dd5f4b3b812bef823d9778
Andrea del Rio <adelrio@gmail.com>
2018-05-14 14:05:55 -0700

    add tests

--- test/test.js
+++ test/test.js
@@ -1,15 +1,25 @@
-var fst = require('../lib');
-var assert = require('assert');
+const fst = require('../lib');
+const assert = require('assert');
+const tape = require('tape');
 
-let build = new fst.SetBuilder("set.fst");
+tape('build SetBuilder', (t) => {
+    let build = new fst.SetBuilder("set.fst");
+    t.ok(build, "SetBuilder built");
+    t.end();
+})
 
-build.insert("bruce");
-build.insert("clarence");
-build.insert("stevie");
+tape("SetBuilder insertion and Set lookup", (t) => {
+    let build = new fst.SetBuilder("set.fst");
 
-build.finish();
+    build.insert("bruce");
+    build.insert("clarence");
+    build.insert("stevie");
 
-let set = new fst.Set("set.fst");
-for (let name of ["bruce", "clarence", "stevie"]) {
-    assert.ok(set.contains(name));
-}
+    build.finish();
+
+    let set = new fst.Set("set.fst");
+    for (let name of ["bruce", "clarence", "stevie"]) {
+        t.ok(set.contains(name));
+    }
+    t.end();
+})
