a8210f294f18eb888ea64cf0234c81da83d7a91f
Andrea del Rio <adelrio@gmail.com>
2018-05-16 13:35:58 -0700

    fix tape throws;

--- test/test.js
+++ test/test.js
@@ -5,10 +5,10 @@ const tape = require('tape');
 tape('build SetBuilder', (t) => {
     let build = new fst.SetBuilder("set.fst");
     t.ok(build, "SetBuilder built");
-    t.throws(new fst.SetBuilder(), Error);
-    t.throws(new fst.SetBuilder("/etc/passwd"));
-    t.throws(new fst.SetBuilder({}));
-    t.throws(new fst.SetBuilder(7));
+    t.throws(() => { new fst.SetBuilder()});
+    t.throws(() => { new fst.SetBuilder("/etc/passwd")});
+    t.throws(() => { new fst.SetBuilder({})});
+    t.throws(() => { new fst.SetBuilder(7)});
     t.end();
 })
 
@@ -26,9 +26,9 @@ tape("SetBuilder insertion and Set lookup", (t) => {
         t.ok(set.contains(name), "Set contains expected word");
     }
 
-    // t.throws(new fst.Set());
-    // t.throws(new fst.Set("/etc/passwd"));
-    // t.throws(new fst.Set({}));
-    // t.throws(new fst.Set(7));
+    t.throws(() => { new fst.Set() });
+    t.throws(() => { new fst.Set("/etc/passwd") });
+    t.throws(() => { new fst.Set({}) });
+    t.throws(() => { new fst.Set(7) });
     t.end();
 })
